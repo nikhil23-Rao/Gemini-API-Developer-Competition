04baf7744c6971545bed7f26ff7ef5a93fa8bac8
Nikhil Rao <nikhil23.rao@gmail.com>
2024-08-05 12:04:41 -0700

    change way answer key works

--- web/serversideapi/getAnswerKey.ts
+++ web/serversideapi/getAnswerKey.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const getAnswerKey = async (query) => {
   const res = await axios.post(
-    //     process.env.NEXT_PUBLIC_API_URL + "assistimg/",
-    "http://localhost:3001/answerkey",
+    process.env.NEXT_PUBLIC_API_URL + "answerkey",
+    // "http://localhost:3001/answerkey",
     { query },
     {
       headers: {
