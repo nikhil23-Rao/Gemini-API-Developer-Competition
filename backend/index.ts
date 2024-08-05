0ad9a15d9c9e4bffebf6580440f10cf870467484
Nikhil Rao <nikhil23.rao@gmail.com>
2024-08-04 23:09:25 -0700

    fix port

--- backend/index.ts
+++ backend/index.ts
@@ -60,7 +60,7 @@ async function getFirstPage(query: string) {
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
